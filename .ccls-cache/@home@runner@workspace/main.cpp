c3ac9cfabb2b2b0f8ca4d7f2c74612f78e6370ca
QGGB07 <qgb23300@email.vccs.edu>
2025-06-24 02:19:09 +0000

    Updated Ocean Levels Code

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,14 +1,17 @@
-  #include <iostream>
-  #include <iomanip>
-  using namespace std;
+#include <iostream>
+using namespace std;
 
-  int main() {
-      const double riseRate = 1.5; // in mm per year
-      cout << "Year\tRise (mm)\n";
-      cout << fixed << setprecision(1);
-      for (int year = 1; year <= 25; year++) {
-          double rise = year * riseRate;
-          cout << year << "\t" << rise << endl;
-      }
-      return 0;
-  }
+int main() {
+    double riseRate = 1.5;
+    int year = 1;
+
+    while (year <= 25) {
+        double oceanRise = year * riseRate;
+        cout << "Year " << year << ": " << oceanRise << " millimeters" << endl;
+        year = year + 1;
+    }
+
+    return 0;
+}
+
+
